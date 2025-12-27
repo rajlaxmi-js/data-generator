97146649c9fec6b4b3ad169f6f888f757b8f92a9
rajlaxmi <rrajlaxmi400@gmail.com>
2025-12-27 16:58:51 +0530

    add react project

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { useEffect,useState } from "react"
+
+const App = () => {
+    const [joke,setJoke]=useState(null);
+    async function fetching() {
+        let Data = await fetch('https://official-joke-api.appspot.com/random_joke');
+        let Fdata = await Data.json();
+        setJoke(Fdata)
+        
+    }
+    useEffect(()=>{
+        setTimeout(()=>{
+            fetching()
+        },1000)
+    },[])
+  return (
+    <div className="joke">
+      <h1>Joke Generator</h1>
+      { joke === null ?(
+        <h2>loading...</h2>
+      ):(
+          <>
+          <h3>{joke.setup}</h3>
+          <p>{joke.punchline}😁😀😅😆</p>
+       </>
+     )}
+      <button onClick = {fetching}>Get New Joke</button>
+     </div>
+  )
+}
+
+export default App
